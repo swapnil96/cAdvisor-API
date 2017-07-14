a9803d687f2cc7d49c9bc42c7298a45687121ea8
Swapnil <dasswapnil96@gmail.com>
2017-07-14 18:40:16 +0800

    Added driver program

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/swapnil96/cAdvisor-API/utils"
+	"flag"
+	"fmt"
+)
+
+// demonstrates how to use event clients
+func main() {
+	flag.Parse()
+	fmt.Println()
+	utils.Host_spec("http://192.168.99.14:8080/")
+	utils.Host_stat("http://192.168.99.14:8080/", "", 10)
+	utils.Host_cpu("http://192.168.99.14:8080/", "", 11)
+	utils.Host_memory("http://192.168.99.14:8080/", "", 11)
+}
