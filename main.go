212a7e6501400b5a1a8a054dcb244d6fc9b89a6d
Swapnil <dasswapnil96@gmail.com>
2017-07-18 15:51:05 +0800

    Fucntion for swarm node analysis

--- main.go
+++ main.go
@@ -4,7 +4,50 @@ import (
 	"github.com/swapnil96/cAdvisor-API/utils"
 	"flag"
 	"fmt"
+	"log"
+	"os/exec"
 )
+
+func check(err error){
+        if err != nil {
+                log.Fatal(err)
+        }
+}
+
+func run() {
+	cmd := exec.Command("sh", "-c", "docker node ls")
+	stdoutStderr, err := cmd.CombinedOutput()
+	check(err)
+	count := 0
+	flag := 0
+	var ids [10]string
+	temp := ""
+	idx := 0
+	for _, b := range stdoutStderr{
+		if b == ' ' || b == '\t'{
+				if flag == 1{
+								ids[idx] = temp
+								idx++
+				}
+				flag = 0
+				temp = ""
+		}
+		if count != 0 && flag == 1{
+				temp += string(b)
+		}
+		if b == '\n'{
+				count++
+				flag = 1
+		}
+	}
+	for _, node := range ids{
+		cmd = exec.Command("sh", "-c", "docker inspect --format='{{.Status.Addr}}' " + node)
+		stdoutStderr, err := cmd.CombinedOutput()
+		check(err)
+		utils.Host_cpu("http://" + string(stdoutStderr) + "/:8080/", "", 11)
+	}
+}
+
 
 // demonstrates how to use event clients
 func main() {
@@ -14,4 +57,5 @@ func main() {
 	utils.Host_stat("http://192.168.99.14:8080/", "", 10)
 	utils.Host_cpu("http://192.168.99.14:8080/", "", 11)
 	utils.Host_memory("http://192.168.99.14:8080/", "", 11)
+	// run()
 }
